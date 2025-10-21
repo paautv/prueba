e0d44952d5d33cd27700542558e192416c276acb
paautv <160149612+Paautv@users.noreply.github.com>
2025-10-21 13:28:52 +0200

    Finish PromoterList component

--- src/components/PromoterList.tsx
+++ src/components/PromoterList.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Card, CardContent, Typography, Box, Link } from "@mui/material";
+import { Card, CardContent, Typography, Box, Link, Button, Avatar } from "@mui/material";
 
 interface Promoter {
   _id: string;
   name: string;
+  logo?: string;
   links: { link: string; type: string }[];
 }
 
@@ -14,19 +15,32 @@ interface PromoterListProps {
 const PromoterList: React.FC<PromoterListProps> = ({ promoters }) => {
   return (
     <Box>
-      <Typography variant="h6" gutterBottom>Promotores</Typography>
+      <Typography variant="h5" gutterBottom>Promotores</Typography>
+      {/* Lista de promotores */}
       {promoters.map(p => (
         <Card key={p._id} sx={{ mb: 2 }}>
-          <CardContent>
-            <Typography variant="subtitle1">{p.name}</Typography>
-            {p.links.map(l => (
-              <Link href={l.link} target="_blank" key={l.link} sx={{ mr: 1 }}>
-                {l.type}
-              </Link>
-            ))}
+          <CardContent sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+            {p.logo && (
+              <Avatar
+                src={`https://api.crowmie.com/api/v1/promoter/content/${p.logo}`}
+                alt={p.name}
+                sx={{ width: 60, height: 60 }}
+              />
+            )}
+            <Box sx={{ flex: 1 }}>
+              <Typography variant="subtitle1">{p.name}</Typography>
+              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1 }}>
+                {p.links.map(l => (
+                  <Link href={l.link} target="_blank" key={l.link}>
+                    {l.type}
+                  </Link>
+                ))}
+              </Box>
+            </Box>
           </CardContent>
         </Card>
       ))}
+      <Button variant="contained" color="primary" sx={{ mt: 3 }}>Invertir</Button>
     </Box>
   );
 };
