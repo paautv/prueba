76a3d3ecc2e387f72d724770524c54e413e51254
paautv <160149612+Paautv@users.noreply.github.com>
2025-10-21 13:28:33 +0200

    Finish ProjectMap component

--- src/components/ProjectMap.tsx
+++ src/components/ProjectMap.tsx
@@ -1,20 +1,33 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import ReactDOMServer from "react-dom/server";
+import L from "leaflet";
 
 interface ProjectMapProps {
-  latitude: number;
   longitude: number;
+  latitude: number;
   name: string;
 }
 
-const ProjectMap: React.FC<ProjectMapProps> = ({ latitude, longitude, name }) => {
+const ProjectMap: React.FC<ProjectMapProps> = ({ longitude, latitude, name }) => {
+  const iconHtml = ReactDOMServer.renderToString(
+    <LocationOnIcon style={{ fontSize: 40 }} />
+  );
+
+  const customIcon = L.divIcon({
+    html: iconHtml,
+    className: "custom-icon",
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
   return (
-    <MapContainer center={[latitude, longitude]} zoom={13} style={{ height: 300, width: '100%', marginBottom: 20 }}>
+    <MapContainer center={[longitude, latitude]} zoom={13} style={{ height: 400, width: '100%', marginBottom: 20 }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[latitude, longitude]}>
+      <Marker position={[longitude, latitude]} icon={customIcon}>
         <Popup>{name}</Popup>
       </Marker>
     </MapContainer>
